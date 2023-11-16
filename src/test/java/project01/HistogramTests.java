9eb81e7ab76da960f966fd44df871606687514c1
Jiacheng <chenjiacheng123456@gmail.com>
2023-11-16 01:59:11 +0100

    added annotations for parameters and assertions in the methods

--- src/test/java/project01/HistogramTests.java
+++ src/test/java/project01/HistogramTests.java
@@ -2,6 +2,9 @@ package project01;
 
 import java.util.List;
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.constraints.Positive;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,31 +22,33 @@ public class HistogramTests {
         Histogram histogram = new Histogram(2, 3, 0, 5, 5, 6, -2, 9, 0, 3, 5);
         Assertions.assertEquals(-2, histogram.min());
         Assertions.assertEquals(9, histogram.max());
+        Assertions.assertEquals(3,histogram.min());
 
-        // TODO: add some more checks here
         Assertions.assertEquals(2, histogram.count(0));
         Assertions.assertEquals(0, histogram.count(1));
         Assertions.assertEquals(1, histogram.count(2));
-        // TODO: add some more checks here
+        Assertions.assertEquals(2,histogram.count(5));
     }
 
     int countOccurrences(int value, List<Integer> data) {
         return (int) data.stream().filter(i -> i == value).count();
     }
 
-    @Property // TODO: specify jqwik annotations for the parameters, note we have conveniently marked with ??? where to insert code
-    void histogramDoesNotCrash(/* ??? */ List</* ??? */ Integer> data) {
+    @Property //
+    void histogramDoesNotCrash(@ForAll List<@Positive Integer> data) {
         new Histogram(data);
     }
 
-    @Property // TODO: specify jqwik annotations for the parameters
-    void histogramCount(List<Integer> data, int value) {
-        // TODO: check method count of class Histogram against reference implementation countOccurrences
+    @Property // correspnds to A2-2 to value from -3 to 10
+    void histogramCount(@ForAll List<@IntRange(min=-3,max=10) Integer> data, @ForAll @IntRange(min=-3,max=10) int value)
+    {
+        Assertions.assertEquals(new Histogram(data).count(value),countOccurrences(value,data));
     }
 
-    @Property // TODO: specify jqwik annotations for the parameters
-    void histogramRange(List<Integer> data, int value) {
-        // TODO: check that if countOccurrences(value) > 0 then
-        //       value is between min and max of the corresponding histogram
+    @Property
+    void histogramRange(@ForAll List<@IntRange(min=-3,max=10) Integer> data, @ForAll @IntRange(min=-3,max=10) int value) {
+        Assertions.assertTrue(countOccurrences(value,data)>0);
+        Assertions.assertTrue(value <= new Histogram(data).max );
+        Assertions.assertTrue(value >= new Histogram(data).min);
     }
 }
